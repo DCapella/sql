73333895953d6f98451d83388ffdc11039f948bd
David Capella <D_Capella@yahoo.com>
2019-01-12 14:11:52 -0600

    finished with practice on WITH

--- q_4/q_4_13.sql
+++ q_4/q_4_13.sql
@@ -49,6 +49,9 @@ SELECT t3.name AS name, t2.region AS region, t2.total_usd AS total_usd
 FROM t2
 JOIN t3
 ON t3.total_usd = t2.total_usd
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+=============================================================================
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
 -- For the region with the largest sales total_amt_usd, how many total orders were placed?
 --
@@ -105,10 +108,59 @@ FROM t1
 JOIN t2
 ON t1.region = t2.region
 
--- For the name of the account that purchased the most (in total over their lifetime as a customer) standard_qty paper, how many accounts still had more in total purchases?
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+=============================================================================
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+-- For the name of the account that purchased the most (in total over their lifetime as a customer)
+-- standard_qty paper, how many accounts still had more in total purchases?
 --
+SELECT COUNT(*)
+FROM (SELECT a.name AS name, SUM(o.total) AS total_amt
+  FROM accounts AS a
+  JOIN orders AS o
+  ON o.account_id = a.id
+  GROUP BY 1
+  HAVING SUM(o.total) > (SELECT MAX(total_amt)
+    FROM (SELECT a.name AS name, SUM(o.standard_qty) AS total_standard, SUM(o.total) AS total_amt
+      FROM accounts AS a
+      JOIN orders AS o
+      ON o.account_id = a.id
+      GROUP BY 1
+      ORDER BY 2 DESC
+      LIMIT 1) AS t1)) AS t2
+
+WITH t1 AS (
+  SELECT a.name AS name, SUM(o.standard_qty) AS total_standard, SUM(o.total) AS total_amt
+  FROM accounts AS a
+  JOIN orders AS o
+  ON o.account_id = a.id
+  GROUP BY 1
+  ORDER BY 2 DESC
+  LIMIT 1),
+t2 AS (
+  SELECT a.name AS name, SUM(o.total) AS total_amt
+  FROM accounts AS a
+  JOIN orders AS o
+  ON o.account_id = a.id
+  GROUP BY 1
+  HAVING SUM(o.total) > (SELECT MAX(total_amt)
+    FROM t1))
+
+SELECT COUNT(*)
+FROM t2
+
+
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+=============================================================================
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 -- For the customer that spent the most (in total over their lifetime as a customer) total_amt_usd, how many web_events did they have for each channel?
 --
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+=============================================================================
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 -- What is the lifetime average amount spent in terms of total_amt_usd for the top 10 total spending accounts?
 --
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+=============================================================================
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 -- What is the lifetime average amount spent in terms of total_amt_usd for only the companies that spent more than the average of all accounts.
